2ae8a2a42289d8307d184bb0bb187df7e932b2f3
EdsonCuevas <efelix@ucol.mx>
2023-11-16 07:27:32 -0600

    Add funcion level2

--- GameEasy.py
+++ GameEasy.py
@@ -40,7 +40,37 @@ def cinematica1():
             if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.KEYDOWN:
                 #Cierra el video
                 run = False
-        
+
+def cinematica2():
+
+    cinematic2 = Video("assets/cinematics/Cinematic2.mp4")
+    cinematic2.set_size((1280, 720))
+
+    #Pantalla
+    W,H = 1280,720
+    icon = pygame.image.load("img/Sprites/FishAnimation/fish1.png")
+    PANTALLA = pygame.display.set_mode((W,H))
+    pygame.display.set_caption("Sea Heroes")
+    pygame.display.set_icon(icon)
+
+    run = True
+    while cinematic2.active and run == True:
+        cinematic2.draw(PANTALLA, (0,0))
+
+        if cinematic2.draw(PANTALLA, (0, 0), force_draw=False):
+            pygame.display.update()
+            
+
+        #Bucle for para que la ventana del juego se pueda cerrar
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            #Detecta si el mouse se presiona
+            if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.KEYDOWN:
+                #Cierra el video
+                run = False
+
 def load_level1():
 
     #Carga el JSON
