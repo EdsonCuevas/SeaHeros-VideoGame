8a1ef7c5b597f49c3c1f15f6119d60bad95aa61e
EdsonCuevas <efelix@ucol.mx>
2023-11-23 12:26:08 -0600

    Game Finished

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt-get install python3-pygame
+sudo install python3-pip
+pip install ffpyplayer
+pip install pymediainfo
+sudo apt-get update -y
+sudo apt-get install -y libmediainfo-dev
